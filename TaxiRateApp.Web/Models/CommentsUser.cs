ad3de1452478ed5124b9ded50b5f1483cbf0a183
Ömer Özkurt <ozkurtomer@outlook.com>
2021-11-16 00:57:42 +0300

    -Düzenlemeler

--- TaxiRateApp.Web/Models/CommentsUser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TaxiRateApp.Web.Models
-{
-    public class CommentsUser
-    {
-        public int Comment_Id { get; set; }
-        public string Comment_UserName { get; set; }
-    }
-}

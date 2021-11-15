086079fef8b68a7e03248be7853a6080687089f1
Ömer Özkurt <ozkurtomer@outlook.com>
2021-11-16 00:53:54 +0300

    - Register tamamlandı

---
+++ TaxiRateApp.Web/Controllers/RegisterController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+using TaxiRateApp.Business.Abstract;
+using TaxiRateApp.Business.Concrete;
+using TaxiRateApp.Core.Utilities.Security;
+using TaxiRateApp.Entities.Dtos;
+
+namespace TaxiRateApp.Web.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(UserForRegisterDto userForRegisterDto)
+        {
+            var client = new RestClient($"https://localhost:44327/api/auth/register");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("content-type", "application/json");
+
+            request.AddParameter("application/json", JsonConvert.SerializeObject(userForRegisterDto), ParameterType.RequestBody);
+            var result = client.Execute(request).Content;
+
+            return RedirectToAction("Index", "Posts");
+        }
+    }
+}

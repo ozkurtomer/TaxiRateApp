9b1b64eec63de90f47c683bfb006ac75d41a4dd2
Ömer Özkurt <ozkurtomer@outlook.com>
2022-01-23 01:46:38 +0300

    -Fix

---
+++ TaxiRateApp.Web/Views/Cities/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TaxiRateApp.Web.Views.Cities
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
